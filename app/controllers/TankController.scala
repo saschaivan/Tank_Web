74df07e261afcee5e7f247dfdfb28fa984b87618
saschaivan <sascha.ivan99@gmail.com>
2020-11-04 13:35:00 +0100

    added more routes and tank.scala.hmtl and the about page for the game

--- app/controllers/TankController.scala
+++ app/controllers/TankController.scala
@@ -3,18 +3,49 @@ package controllers
 import com.google.inject.Inject
 import play.api.mvc.{AbstractController, ControllerComponents}
 import de.htwg.se.Tank.{Tank, controller}
+import de.htwg.se.Tank.model.gameComponent.gameBase.Map
 
 class TankController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
   val gamecontroller = Tank.controller
-
+  var map : Map.type = Map
   gamecontroller.setDefaultGame()
-  def tankastext = gamecontroller.gametoString
 
   def about = Action {
     Ok(views.html.index())
   }
 
   def tank = Action {
-    Ok(tankastext)
+    Ok(views.html.tank(gamecontroller))
   }
+
+  def moveleft = Action {
+    gamecontroller.moveLeft()
+    Ok(views.html.tank(gamecontroller))
+  }
+
+  def moveRight = Action {
+    gamecontroller.moveRight()
+    Ok(views.html.tank(gamecontroller))
+  }
+
+  def shoot = Action {
+    gamecontroller.shoot(5)
+    Ok(views.html.tank(gamecontroller))
+  }
+
+  def moveAngleUp = Action {
+    gamecontroller.moveAngleUp()
+    Ok(views.html.tank(gamecontroller))
+  }
+
+  def moveAngleDown = Action {
+    gamecontroller.moveAngleUp()
+    Ok(views.html.tank(gamecontroller))
+  }
+
+  def changePlayer = Action {
+    gamecontroller.changePlayer()
+    Ok(views.html.tank(gamecontroller))
+  }
+
 }
