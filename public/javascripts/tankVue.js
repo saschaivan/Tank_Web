54e426e253120ec02319f54255b60d45fbe772ed
zzzzzyue <xyyxyyxyywww@gmail.com>
2021-01-07 10:52:40 +0100

    some js

--- public/javascripts/tankVue.js
+++ public/javascripts/tankVue.js
@@ -1,72 +1,32 @@
-$(document).ready(function () {
-    var tankGame = new Vue({
-        el:'#tankGame'
-    })
-})
-
-Vue.component('tank-game', {
+// Vue components
+Vue.component('player-name', {
     template:`
-        <div class="map">
-        <canvas id="canvas" width="1100" height="600"></canvas>
+    <div class="input-group mb-3 input-name">
+        <div id="data_player1">
+            <input v-model="player_data.player1" id="player1_name" type="text" class="form-control" placeholder="SPIELER 1">
         </div>
-    `,
-});
-
-Vue.component('tank-button', {
-    template:`
-        <div class="col-9 gameplay_buttons">
-         <button type="sumbit" onclick="moveLeft()" >Left</button>
-         <button type="sumbit" onclick="moveRight()" >Right</button>
+        <div id="data_player2">
+            <input v-model="player_data.player2" id="player2_name" type="text" class="form-control" placeholder="SPIELER 2">
         </div>
-    `,
-});
-
-Vue.component('tank-player', {
-    template:`
-        <div id="player1">
-                    <!-- wird durch tank.js gefüllt -->
+        <div id="test">
+            <p>Name: {{ player_data.player1 }}</p>
+            <p>Name: {{ player_data.player2 }}</p>
         </div>
-        <div id="player2">
-                    <!-- wird durch tank.js gefüllt -->
-        </div>
+    </div>
     `,
+    data() {
+        return {
+            player_data: {
+                player1: "",
+                player2: ""
+            }
+        }
+    }
 });
 
-Vue.component('tank-title', {
-    template:`
-        <h1>
-             Tank
-        </h1>
-    `,
+$(document).ready(function() {
+    var tankmenu = new Vue({
+        el: '#vue'
+    });
 });
-
-Vue.component('tank-title', {
-    template:`
-        <form action="" method="get">
-            <div class="row">
-                <div class="input-group mb-3 input-name">
-                    <form id="data_player1">
-                        <input id="player1_name" type="text" class="form-control" placeholder="SPIELER 1">
-                    </form>
-                    <form id="data_player2">
-                        <input id="player2_name" type="text" class="form-control" placeholder="SPIELER 2">
-                    </form>
-                </div>
-
-                <div class="col-md-2 dropdown">
-                    <button class="btn btn-secondary dropdown-toggle" type="button" id="map"
-                        id="dropdownMenuButton" data-toggle="dropdown">
-                         Map
-                    </button>
-                    <ul class="dropdown-menu" role="menu" id="mapvalue" aria-labelledby="dropdownMenuButton">
-                        <li>Wolfsschanze</li>
-                        <li>Gerade</li>
-                        <li>Sägezahnhügel</li>
-                    </ul>
-                </div>
-            </div>
-        </form>
-    `,
-});
-
 
